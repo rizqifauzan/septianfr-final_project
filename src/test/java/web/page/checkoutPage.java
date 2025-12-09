dca33c0387b29f31605a77e0a134598059a504c1
septianfr <septianfr50@gmail.com>
2025-12-09 21:10:36 +0700

    Add fix for CI

--- src/test/java/web/page/checkoutPage.java
+++ src/test/java/web/page/checkoutPage.java
@@ -72,23 +72,37 @@ public class checkoutPage {
 
     public void inputFirstName(String firstName){
         WebElement first = wait.until(ExpectedConditions.visibilityOfElementLocated(enterFirstName));
+        first.click();
         first.clear();
         first.sendKeys(firstName);
+
+        System.out.println("DEBUG: AFTER FIRST TYPE = " + first.getAttribute("value"));
     }
 
     public void inputLastName(String lastName){
         WebElement last = wait.until(ExpectedConditions.visibilityOfElementLocated(enterLastName));
+        last.click();
         last.clear();
         last.sendKeys(lastName);
+
+        System.out.println("DEBUG: AFTER LAST TYPE = " + last.getAttribute("value"));
     }
 
     public void inputZipCode(int zipCode){
         WebElement zip = wait.until(ExpectedConditions.visibilityOfElementLocated(enterZipCode));
+        zip.click();
         zip.clear();
         zip.sendKeys(String.valueOf(zipCode));
+
+        System.out.println("DEBUG: AFTER ZIP TYPE = " + zip.getAttribute("value"));
     }
 
     public void clickContinueButton(){
+        System.out.println("CHECK BEFORE CLICK:");
+        System.out.println("FIRST NAME VALUE: " + driver.findElement(enterFirstName).getAttribute("value"));
+        System.out.println("LAST NAME VALUE: " + driver.findElement(enterLastName).getAttribute("value"));
+        System.out.println("ZIP CODE VALUE: " + driver.findElement(enterZipCode).getAttribute("value"));
+
         WebElement btn = wait.until(ExpectedConditions.visibilityOfElementLocated(continueBtn));
 
         wait.until(ExpectedConditions.elementToBeClickable(btn));
